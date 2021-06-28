69fbf51a786a909fb9b42bcac468eac4d7e5396a
Julien MARCEL <jul_marcel@hotmail.com>
2021-06-28 14:17:41 +0200

    issue #1 : fermeture modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,23 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.querySelector("#bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const fermeModalBtn = document.querySelectorAll("#close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Evenement ferme la modale
+fermeModalBtn.forEach(elt => elt.addEventListener("click", fermeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// ferme la modale
+function fermeModal() {
+  modalbg.style.display = "none";
+}
